faceb4db8feab3ea3443c8f433bb3b884edcdf6f
David Middlecamp <david@spark.io>
2017-04-13 19:49:08 -0500

    notes

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # particle_arducam
+
+
+how to build
+---
+
+    ./build.sh sandbox
+
+
+how to flash
+---
+
+    ./flash.sh sandbox
+
+
+how to see if it's working
+---
+
+    particle serial monitor --follow
+
+Hoping to see something like "Camera found, initializing..."
+
+
+how it's wired
+---
+
+This is probably wrong, but it's what I have so far, feedback very welcome.
+
+Photon -- Camera -- Purpose
+A2 -- CS -- SS
+
+|Photon	|Camera	|Purpose|
+|---	|---	|---	|
+|A2   	|CS   	|SS   	|
+|A3   	|SCK   	|SCK   	|
+|A4   	|MOSI  	|MOSI  	|
+|A5   	|MISO  	|MISO  	|
+
--- firmware/sandbox/main.ino
+++ firmware/sandbox/main.ino
@@ -39,6 +39,8 @@ void setup()
 
   // initialize SPI:
   SPI.begin();
+  //SPI.begin(SPI_MODE_MASTER);
+  //SPI.begin(SPI_MODE_SLAVE, SPI_CS);
 
 
   while(1) {
@@ -58,6 +60,34 @@ void setup()
     }
   }
 
+//NOTES
+//   while (1) {
+//    //Check if the camera module type is OV5642
+//    myCAM1.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+//    myCAM1.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+//    if ((vid != 0x56) || (pid != 0x42)) {
+//      Serial.println(F("Can't find OV5642 module!"));
+//      delay(1000); continue;
+//    } else {
+//      Serial.println(F("OV5642 detected.")); break;
+//    }
+//  }
+//
+//Change to JPEG capture mode and initialize the OV5640 module
+//  myCAM1.set_format(JPEG);
+//  myCAM1.InitCAM();
+//  myCAM1.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
+//  myCAM1.clear_fifo_flag();
+//  myCAM1.write_reg(ARDUCHIP_FRAMES, FRAMES_NUM);
+//
+//  //myCAM1.OV5642_set_JPEG_size(OV5642_640x480); delay(1000);
+//  myCAM1.OV5642_set_JPEG_size(OV5642_320x240); delay(1000);
+//  delay(1000);
+//  myCAM1.clear_fifo_flag();
+//  Serial.println("Ready:,1");
+
+
+
   Serial.println("Camera found, initializing...");
 
   //Change MCU mode
@@ -67,140 +97,63 @@ void setup()
 
 void loop()
 {
-  char str[8];
-  unsigned long previous_time = 0;
-  static int k = 0;
-  uint8_t temp;
-
-  myCAM.write_reg(ARDUCHIP_MODE, 0x01);		 	//Switch to CAM
-
-  while(1)
-  {
-    temp = myCAM.read_reg(ARDUCHIP_TRIG);
-
-    if(!(temp & VSYNC_MASK))				//New Frame is coming
-    {
-       myCAM.write_reg(ARDUCHIP_MODE, 0x00);    	//Switch to MCU
-//       myGLCD.resetXY();
-       myCAM.write_reg(ARDUCHIP_MODE, 0x01);    	//Switch to CAM
-       while(!(myCAM.read_reg(ARDUCHIP_TRIG)&0x01));	//Wait for VSYNC is gone
-    }
-	else if ((temp & SHUTTER_MASK))
-	{
-       previous_time = millis();
-       while(myCAM.read_reg(ARDUCHIP_TRIG) & SHUTTER_MASK)
-       {
-         if((millis() - previous_time) > 1500)
-         {
-           Playback();
-         }
-       }
-       if((millis() - previous_time) < 1500)
-       {
-         k = k + 1;
-         itoa(k, str, 10);
-         strcat(str,".bmp");				//Generate file name
-         myCAM.write_reg(ARDUCHIP_MODE, 0x00);    	//Switch to MCU, freeze the screen
-         GrabImage(str);
-       }
-    }
-  }
-}
-
-
-void GrabImage(char* str)
-{
-
-//  File outFile;
-  char VH,VL;
-  uint8_t temp;
-  byte buf[256];
-  static int k = 0;
-  int i,j = 0;
-
-//  outFile = SD.open(str,O_WRITE | O_CREAT | O_TRUNC);
-//  if (! outFile)
+//  char str[8];
+//  unsigned long previous_time = 0;
+//  static int k = 0;
+//  uint8_t temp;
+//
+//  myCAM.write_reg(ARDUCHIP_MODE, 0x01);		 	//Switch to CAM
+//
+//  while(1)
 //  {
-//    Serial.println("Open File Error");
-//    return;
+//    temp = myCAM.read_reg(ARDUCHIP_TRIG);
+//
+//    if(!(temp & VSYNC_MASK))				//New Frame is coming
+//    {
+//       myCAM.write_reg(ARDUCHIP_MODE, 0x00);    	//Switch to MCU
+////       myGLCD.resetXY();
+//       myCAM.write_reg(ARDUCHIP_MODE, 0x01);    	//Switch to CAM
+//       while(!(myCAM.read_reg(ARDUCHIP_TRIG)&0x01));	//Wait for VSYNC is gone
+//    }
+//	else if ((temp & SHUTTER_MASK))
+//	{
+//       previous_time = millis();
+//       while(myCAM.read_reg(ARDUCHIP_TRIG) & SHUTTER_MASK)
+//       {
+//         if((millis() - previous_time) > 1500)
+//         {
+//           Playback();
+//         }
+//       }
+//       if((millis() - previous_time) < 1500)
+//       {
+//         k = k + 1;
+//         itoa(k, str, 10);
+//         strcat(str,".bmp");				//Generate file name
+//         myCAM.write_reg(ARDUCHIP_MODE, 0x00);    	//Switch to MCU, freeze the screen
+//         GrabImage(str);
+//       }
+//    }
 //  }
-
-  //Switch to FIFO Mode
-  myCAM.write_reg(ARDUCHIP_TIM, MODE_MASK);
-  //Flush the FIFO
-  myCAM.flush_fifo();
-  //Start capture
-  myCAM.start_capture();
-  Serial.println("Start Capture");
-
-  //Polling the capture done flag
-  while(!(myCAM.read_reg(ARDUCHIP_TRIG) & CAP_DONE_MASK));
-  Serial.println("Capture Done!");
-
-  k = 0;
-  //Write the BMP header
-  for( i = 0; i < BMPIMAGEOFFSET; i++)
-  {
-    char ch = pgm_read_byte(&bmp_header[i]);
-    buf[k++] = ch;
-  }
-  //outFile.write(buf,k);
-
-  //------------------------------
-  //---Serial print here    outFile.write(buf,k);
-  //------------------------------
-
-  //Read first dummy byte
-  //myCAM.read_fifo();
-
-  k = 0;
-  //Read 320x240x2 byte from FIFO
-  //Save as RGB565 bmp format
-  for(i = 0; i < 240; i++)
-    for(j = 0; j < 320; j++)
-  {
-      VH = myCAM.read_fifo();
-      VL = myCAM.read_fifo();
-      buf[k++] = VL;
-      buf[k++] = VH;
-      //Write image data to bufer if not full
-      if(k >= 256)
-      {
-        //Write 256 bytes image data to file from buffer
-        //outFile.write(buf,256);
-          //------------------------------
-          //---Serial print here    outFile.write(buf,256);
-          //------------------------------
-
-        k = 0;
-      }
-  }
-  //Close the file
-  //outFile.close();
-  //Clear the capture done flag
-  myCAM.clear_fifo_flag();
-
-  //Switch to LCD Mode
-  myCAM.write_reg(ARDUCHIP_TIM, 0);
-  return;
 }
-
+//
+//
 //void GrabImage(char* str)
 //{
 //
-//  File outFile;
+////  File outFile;
 //  char VH,VL;
 //  uint8_t temp;
 //  byte buf[256];
 //  static int k = 0;
 //  int i,j = 0;
 //
-//  outFile = SD.open(str,O_WRITE | O_CREAT | O_TRUNC);
-//  if (! outFile)
-//  {
-//    Serial.println("Open File Error");
-//    return;
-//  }
+////  outFile = SD.open(str,O_WRITE | O_CREAT | O_TRUNC);
+////  if (! outFile)
+////  {
+////    Serial.println("Open File Error");
+////    return;
+////  }
 //
 //  //Switch to FIFO Mode
 //  myCAM.write_reg(ARDUCHIP_TIM, MODE_MASK);
@@ -221,7 +174,12 @@ void GrabImage(char* str)
 //    char ch = pgm_read_byte(&bmp_header[i]);
 //    buf[k++] = ch;
 //  }
-//  outFile.write(buf,k);
+//  //outFile.write(buf,k);
+//
+//  //------------------------------
+//  //---Serial print here    outFile.write(buf,k);
+//  //------------------------------
+//
 //  //Read first dummy byte
 //  //myCAM.read_fifo();
 //
@@ -239,12 +197,16 @@ void GrabImage(char* str)
 //      if(k >= 256)
 //      {
 //        //Write 256 bytes image data to file from buffer
-//        outFile.write(buf,256);
+//        //outFile.write(buf,256);
+//          //------------------------------
+//          //---Serial print here    outFile.write(buf,256);
+//          //------------------------------
+//
 //        k = 0;
 //      }
 //  }
 //  //Close the file
-//  outFile.close();
+//  //outFile.close();
 //  //Clear the capture done flag
 //  myCAM.clear_fifo_flag();
 //
@@ -252,32 +214,100 @@ void GrabImage(char* str)
 //  myCAM.write_reg(ARDUCHIP_TIM, 0);
 //  return;
 //}
-
-
-void Playback()
-{
-/*
-  File inFile;
-  char str[8];
-  int k = 0;
-  myCAM.write_reg(ARDUCHIP_MODE, 0x00);    		//Switch to MCU
-//  myGLCD.InitLCD(PORTRAIT);
-
-  while(1)
-  {
-      k = k + 1;
-      itoa(k, str, 10);
-      strcat(str,".bmp");
-      inFile = SD.open(str,FILE_READ);
-      if (! inFile)
-        return;
-      myGLCD.clrScr();
-      //myGLCD.resetXY();
-      myGLCD.dispBitmap(inFile);
-      inFile.close();
-      delay(2000);
-  }
-*/
-}
-
+//
+////void GrabImage(char* str)
+////{
+////
+////  File outFile;
+////  char VH,VL;
+////  uint8_t temp;
+////  byte buf[256];
+////  static int k = 0;
+////  int i,j = 0;
+////
+////  outFile = SD.open(str,O_WRITE | O_CREAT | O_TRUNC);
+////  if (! outFile)
+////  {
+////    Serial.println("Open File Error");
+////    return;
+////  }
+////
+////  //Switch to FIFO Mode
+////  myCAM.write_reg(ARDUCHIP_TIM, MODE_MASK);
+////  //Flush the FIFO
+////  myCAM.flush_fifo();
+////  //Start capture
+////  myCAM.start_capture();
+////  Serial.println("Start Capture");
+////
+////  //Polling the capture done flag
+////  while(!(myCAM.read_reg(ARDUCHIP_TRIG) & CAP_DONE_MASK));
+////  Serial.println("Capture Done!");
+////
+////  k = 0;
+////  //Write the BMP header
+////  for( i = 0; i < BMPIMAGEOFFSET; i++)
+////  {
+////    char ch = pgm_read_byte(&bmp_header[i]);
+////    buf[k++] = ch;
+////  }
+////  outFile.write(buf,k);
+////  //Read first dummy byte
+////  //myCAM.read_fifo();
+////
+////  k = 0;
+////  //Read 320x240x2 byte from FIFO
+////  //Save as RGB565 bmp format
+////  for(i = 0; i < 240; i++)
+////    for(j = 0; j < 320; j++)
+////  {
+////      VH = myCAM.read_fifo();
+////      VL = myCAM.read_fifo();
+////      buf[k++] = VL;
+////      buf[k++] = VH;
+////      //Write image data to bufer if not full
+////      if(k >= 256)
+////      {
+////        //Write 256 bytes image data to file from buffer
+////        outFile.write(buf,256);
+////        k = 0;
+////      }
+////  }
+////  //Close the file
+////  outFile.close();
+////  //Clear the capture done flag
+////  myCAM.clear_fifo_flag();
+////
+////  //Switch to LCD Mode
+////  myCAM.write_reg(ARDUCHIP_TIM, 0);
+////  return;
+////}
+//
+//
+//void Playback()
+//{
+///*
+//  File inFile;
+//  char str[8];
+//  int k = 0;
+//  myCAM.write_reg(ARDUCHIP_MODE, 0x00);    		//Switch to MCU
+////  myGLCD.InitLCD(PORTRAIT);
+//
+//  while(1)
+//  {
+//      k = k + 1;
+//      itoa(k, str, 10);
+//      strcat(str,".bmp");
+//      inFile = SD.open(str,FILE_READ);
+//      if (! inFile)
+//        return;
+//      myGLCD.clrScr();
+//      //myGLCD.resetXY();
+//      myGLCD.dispBitmap(inFile);
+//      inFile.close();
+//      delay(2000);
+//  }
+//*/
+//}
+//
 
